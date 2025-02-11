7f2b1616e21c8a7e4a31165d267311730de4052a
gabu322 <gabrieldallorto@hotmail.com>
2025-02-11 02:45:19 -0300

    Created Current user page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,147 @@
 "use client";
 
 import Button from "@/components/Button";
-import { useState } from "react";
+import Down from "@/public/icons/down";
+import axios from "axios";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-   const [suggestedUsers, setSuggestedUsers] = useState([1]);
+   const [currentUser, setCurrentUser] = useState<any>();
+   const [suggestedUsers, setSuggestedUsers] = useState<number[]>([1]);
+   const [personalInfoOpen, setPersonalInfoOpen] = useState<boolean>(false);
+   const [contactInfoOpen, setContactInfoOpen] = useState<boolean>(false);
+
+   useEffect(() => {
+      axios.get("https://randomuser.me/api/").then((res) => {
+         setCurrentUser({ seed: res.data.info.seed, ...res.data.results[0] });
+         console.log({ seed: res.data.info.seed, ...res.data.results[0] });
+      });
+   }, []);
 
    return (
       <div className="flex flex-col gap-4">
+         <h1 className="text-3xl md:text-4xl font-semibold">Find new users like you</h1>
+
          {/* Current user info */}
-         <h1 className="text-4xl font-semibold">Find new users like you</h1>
+         <div className="card relative overflow-hidden h-96">
+            <div className="relative w-full h-1/2">
+               {currentUser && (
+                  <Image
+                     src={currentUser?.picture.large}
+                     alt="Background image"
+                     fill
+                     style={{ objectFit: "cover" }}
+                     className="filter blur-sm"
+                  />
+               )}
+            </div>
 
-         <div className="card">Person</div>
+            {currentUser && (
+               <Image
+                  src={currentUser?.picture.large}
+                  alt="Front image"
+                  width={200}
+                  height={200}
+                  className="absolute rounded-full border-4 border-white object-cover 
+                     left-1/2 top-10 -translate-x-1/2 shadow-md"
+               />
+            )}
 
-         <div className="flex flex-col md:flex-row gap-4">
-            <div className="card flex-1">Personal Info</div>
+            <div className="px-4 pt-12 pb-4 flex flex-col w-full items-center h-1/2">
+               <h3 className="font-bold text-2xl">
+                  {currentUser?.name.first} {currentUser?.name.last}
+               </h3>
 
-            <div className="card flex-1">Contact info</div>
+               <h4 className=" text-xl">
+                  {currentUser?.location.city}, {currentUser?.location.country}
+               </h4>
+
+               <div className="flex flex-row gap-4 w-full mt-auto">
+                  <Button
+                     text="Follow"
+                     className="flex-1"
+                  />
+                  <Button
+                     text="Try next user"
+                     bgColor="bg-slate-100"
+                     hoverColor="hover:bg-slate-200"
+                     textColor="black"
+                     className="flex-1"
+                  />
+               </div>
+            </div>
+         </div>
+
+         {/* Personal and contact info, needed for responsivity */}
+         <div className="flex flex-col md:flex-row md:items-start gap-4">
+            {/* Pesonal info */}
+            <div className="card flex-1 flex flex-col gap-4 p-4">
+               <h3 className="font-bold text-2xl">Personal Info</h3>
+
+               <div className="flex flex-col gap-2 pl-2">
+                  <h4 className="text-lg">Born at: {currentUser?.nat}</h4>
+
+                  <h4 className="text-lg">Age: {currentUser?.dob.age}</h4>
+
+                  <div className={`flex flex-col gap-2 transition-all duration-500 overflow-hidden ${personalInfoOpen ? "max-h-96 " : "max-h-0 opacity-"}`}>
+                     <h4 className="text-lg">Gender: {currentUser?.gender}</h4>
+                     <h4 className="text-lg">Title: {currentUser?.name.title}</h4>
+                  </div>
+               </div>
+
+               <hr className="" />
+
+               <Button
+                  bgColor="transparent"
+                  hoverColor="hover:bg-gray-200"
+                  textColor="text-blue-500"
+                  rounded
+                  noShadow
+                  textSize="text-lg"
+                  onClick={() => setPersonalInfoOpen(!personalInfoOpen)}
+               >
+                  See more info{" "}
+                  <Down
+                     className={`w-4 h-4 ml-2 transition-transform duration-300 ${personalInfoOpen ? "rotate-180" : ""}`}
+                     color="blue"
+                  />
+               </Button>
+            </div>
+
+            {/* Contact info */}
+            <div className="card flex-1 flex flex-col gap-4 p-4 min-h-0">
+               <h3 className="font-bold text-2xl">Contact Info</h3>
+
+               <div className="flex flex-col gap-2 pl-2">
+                  <h4 className="text-lg">Email: {currentUser?.email}</h4>
+
+                  <h4 className="text-lg">Phone: {currentUser?.phone}</h4>
+
+                  <div className={`flex flex-col gap-2 transition-all duration-500 overflow-hidden ${contactInfoOpen ? "max-h-96 " : "max-h-0 opacity-"}`}>
+                     <h4 className="text-lg">Cell: {currentUser?.cell}</h4>
+                     <h4 className="text-lg">Username: {currentUser?.login.username}</h4>
+                  </div>
+               </div>
+
+               <hr className="" />
+
+               <Button
+                  bgColor="transparent"
+                  hoverColor="hover:bg-gray-200"
+                  textColor="text-blue-500"
+                  rounded
+                  noShadow
+                  textSize="text-lg"
+                  onClick={() => setContactInfoOpen(!contactInfoOpen)}
+               >
+                  See more info{" "}
+                  <Down
+                     className={`w-4 h-4 ml-2 transition-transform duration-300 ${contactInfoOpen ? "rotate-180" : ""}`}
+                     color="blue"
+                  />
+               </Button>
+            </div>
          </div>
 
          {/* Suggested users */}
@@ -26,7 +151,7 @@ export default function Home() {
             {suggestedUsers.map((user) => (
                <UserCard
                   key={user}
-                  className="card"
+                  className="card p-4"
                />
             ))}
          </div>
